d370e6ad9f9f422d46906990086d4305c70a2b41
anirudh22 <anirudhbhatia22@gmail.com>
2025-11-22 06:56:53 +0000

    Fix Jenkins automation

--- run-oneclick-ansible.sh
+++ run-oneclick-ansible.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
 set -e
 
 echo "--------------------------------------"
 echo "1) Reading Terraform outputs"
 echo "--------------------------------------"
 
-# Read outputs
-BASTION_IP=$(terraform -chdir=terraform output -raw bastion_ip)
-PRIMARY_IP=$(terraform -chdir=terraform output -raw primary_db_ip)
-REPLICA_IP=$(terraform -chdir=terraform output -raw replica_db_ip)
+BASTION=$(terraform -chdir=terraform output -raw bastion_ip)
+PRIMARY=$(terraform -chdir=terraform output -raw primary_db_ip)
+REPLICA=$(terraform -chdir=terraform output -raw replica_db_ip)
 
-echo "Bastion: $BASTION_IP"
-echo "Primary: $PRIMARY_IP"
-echo "Replica: $REPLICA_IP"
+echo "Bastion: $BASTION"
+echo "Primary: $PRIMARY"
+echo "Replica: $REPLICA"
 
 echo "--------------------------------------"
 echo "2) Creating hosts.ini automatically"
@@ -21,20 +19,16 @@ echo "--------------------------------------"
 
 cat > ansible/hosts.ini <<EOF
 [db]
-primary ansible_host=$PRIMARY_IP
-replica ansible_host=$REPLICA_IP
+primary ansible_host=$PRIMARY
+replica ansible_host=$REPLICA
 
 [bastion]
-bastion ansible_host=$BASTION_IP
+bastion ansible_host=$BASTION
 
 [all:vars]
 ansible_user=ubuntu
 ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/jenkins.pem
-ansible_ssh_common_args='
-  -o StrictHostKeyChecking=no
-  -o UserKnownHostsFile=/dev/null
-  -o ProxyCommand="ssh -W %h:%p -i /var/lib/jenkins/.ssh/jenkins.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$BASTION_IP"
-'
+ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -W %h:%p -i /var/lib/jenkins/.ssh/jenkins.pem ubuntu@$BASTION"'
 EOF
 
 echo "hosts.ini created:"
